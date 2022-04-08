f97eef85f786393e6d798ecbffa2a00c9aabcdc6
andy <jack.hanzhao@gmail.com>
2022-04-08 11:18:51 +0800

    增加保存文件功能

--- src/BaseShopifyApi.php
+++ src/BaseShopifyApi.php
@@ -2,6 +2,9 @@
 namespace Shopifyapi;
 
 use Illuminate\Config\Repository;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Shopifyapi\Common\Helper;
 
 /**
@@ -38,8 +41,8 @@ abstract class BaseShopifyApi
 
         curl_setopt($ch, CURLOPT_URL, $this->config['api_url'] . $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        // 是否直接显示在控制台
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+        // 是否返回数据，0否，1是
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         if($method == 'POST'){
             curl_setopt($ch, CURLOPT_POST, 1);
@@ -63,6 +66,43 @@ abstract class BaseShopifyApi
         $output = json_decode($output,true);
 
         return Helper::success($output);
+    }
+
+    /**
+     * 下载资源信息
+     * Author: Andy
+     * @param string $url
+     * @return bool|string
+     */
+    protected function download(string $url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $file = curl_exec($ch);
+        curl_close($ch);
+
+        return $file;
+    }
+
+    /**
+     * 保存文件
+     * Author: Andy
+     * @param $file
+     * @return bool
+     */
+    protected function saveFile($file, $ext = 'jpg')
+    {
+        try {
+            $path = sprintf('/shopify/%s/%s.%s', date('Y-m-d'), Str::uuid(), $ext);
+            Storage::disk('local')->put($path, $file);
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage() . "\n" . $e->getLine() . "\n" . $e->getTraceAsString());
+            return false;
+        }
+        return true;
     }
 
 }
--- src/ShopifyApi.php
+++ src/ShopifyApi.php
@@ -10,12 +10,38 @@ namespace Shopifyapi;
 class ShopifyApi extends BaseShopifyApi
 {
     /**
-     * 获取产品信息
+     * 调用接口获取信息
      * Author: Andy
      * @return mixed
      */
-    public function products()
+    public function handler(array $params = [], string $method = 'GET')
     {
-        return $this->curlApi($this->config['api_admin_url']['product']);
+        return $this->curlApi($this->config['api_admin_url']['product'], $params, $method);
+    }
+
+    /**
+     * 下载文件
+     * Author: Andy
+     * @param string $url
+     * @return bool|string
+     */
+    public function downloadFile(string $url)
+    {
+        $file = $this->download($url);
+        $ext = $this->fileExt($url);
+        return $this->saveFile($file, $ext);
+    }
+
+    /**
+     * 获取文件后缀
+     * Author: Andy
+     * @param string $url
+     * @return mixed|string
+     */
+    protected function fileExt(string $url)
+    {
+        $pathInfo = pathinfo($url, PATHINFO_EXTENSION);
+        $pathInfo = explode('?', $pathInfo);
+        return $pathInfo[0] ?? 'jpg';
     }
 }
